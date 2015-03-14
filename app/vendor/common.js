81ade42151fdbca30e6ecde3437d37b03b6e2af4
deiu <andrei@fcns.eu>
2015-03-14 11:29:49 -0400

    Started working on WebID-RSA support. Fixes.

--- app/vendor/common.js
+++ app/vendor/common.js
@@ -53,6 +53,7 @@ function unquote(value) {
   return value;
 }
 
+// parseLinkHeader(xhr..getResponseHeader('Link'))['meta']['href'];
 function parseLinkHeader(header) {
   var linkexp = /<[^>]*>\s*(\s*;\s*[^\(\)<>@,;:"\/\[\]\?={} \t]+=(([^\(\)<>@,;:"\/\[\]\?={} \t]+)|("[^"]*")))*(,|$)/g;
   var paramexp = /[^\(\)<>@,;:"\/\[\]\?={} \t]+=(([^\(\)<>@,;:"\/\[\]\?={} \t]+)|("[^"]*"))/g;
@@ -74,11 +75,32 @@ function parseLinkHeader(header) {
     }
 
     if (link.rel !== undefined) {
-      rels[link.rel] = link;
+      if (!rels[link.rel] || rels[link.rel].length === 0) {
+        rels[link.rel] = [];
+      }
+      rels[link.rel].push(link.href);
     }
   }
 
   return rels;
+}
+
+function PopupCenter(url, title, w, h) {
+    // Fixes dual-screen position                         Most browsers      Firefox
+    var dualScreenLeft = window.screenLeft != undefined ? window.screenLeft : screen.left;
+    var dualScreenTop = window.screenTop != undefined ? window.screenTop : screen.top;
+
+    width = window.innerWidth ? window.innerWidth : document.documentElement.clientWidth ? document.documentElement.clientWidth : screen.width;
+    height = window.innerHeight ? window.innerHeight : document.documentElement.clientHeight ? document.documentElement.clientHeight : screen.height;
+
+    var left = ((width / 2) - (w / 2)) + dualScreenLeft;
+    var top = ((height / 2) - (h / 2)) + dualScreenTop;
+    var newWindow = window.open(url, title, 'scrollbars=yes, width=' + w + ', height=' + h + ', top=' + top + ', left=' + left);
+
+    // Puts focus on the newWindow
+    if (window.focus) {
+        newWindow.focus();
+    }
 }
 
 // notifications
